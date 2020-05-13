c0f299afeb186215cb862f1cd141cb065ebe21d3
Ikeh Victor <victorikeh77@gmail.com>
2020-05-13 22:44:42 +0000

    testing out configs in tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      color: {
+        'brand-blue': '#0a91ab',
+      },
+    },
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+  },
+  plugins: [],
+}
